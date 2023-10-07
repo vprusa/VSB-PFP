1aaa8cceb2d937e5a6b301e4a929bb1618fc3267
Vojtěch Průša <prusa.vojtech@gmail.com>
2023-10-07 20:27:15 +0200

    bkp

--- homeworks/1/h_1.hs
+++ homeworks/1/h_1.hs
@@ -97,26 +97,90 @@ pp x = putStr (concat (map (++"\n") x))
 
 
 
+-- {-# LANGUAGE MultiParamTypeClasses #-}
+-- {-# LANGUAGE FunctionalDependencies #-}
+
+-- -- Define a type class for structures that can contain characters
+-- class CharacterContainer a where
+--   findCharCoordinates :: Char -> a -> [(Int, Int)]
+--   switchCoordinates :: (Int, Int) -> (Int, Int) -> a -> a
+
+-- -- Implement the CharacterContainer type class for a list of strings
+-- instance CharacterContainer [String] where
+--   findCharCoordinates char grid =
+--     [(row, col) | (row, rowString) <- zip [0..] grid, (col, gridChar) <- zip [0..] rowString, gridChar == char]
+  
+--   switchCoordinates (row1, col1) (row2, col2) grid =
+--     let char1 = (grid !! row1) !! col1
+--         char2 = (grid !! row2) !! col2
+--         newRow1 = replaceAtIndex col1 (grid !! row1) char2
+--         newRow2 = replaceAtIndex col2 (grid !! row2) char1
+--         newGrid = replaceAtIndex row1 grid newRow1
+--     in replaceAtIndex row2 newGrid newRow2
+
+-- -- Helper function to replace an element at a specific index in a list
+-- replaceAtIndex :: Int -> [a] -> a -> [a]
+-- replaceAtIndex index xs newElement = take index xs ++ [newElement] ++ drop (index + 1) xs
+
+-- -- Example puzzle data
+-- puzzle2 :: [String]
+-- puzzle2 = ["AC DE",
+--            "FBHIJ",
+--            "KGLNO",
+--            "PQMRS",
+--            "UVWXT"]
+
+-- main :: IO ()
+-- main = do
+--   -- let charToFind = 'H'
+--       -- coordinates = findCharCoordinates charToFind puzzle2
+--   -- putStrLn ("Coordinates of '" ++ [charToFind] ++ "': " ++ show coordinates)
+
+--   -- -- let newGrid = switchCoordinates (1, 2) (2, 2) puzzle2
+--   -- -- let oldCoords = findCharCoordinates ' ' puzzle2
+--   -- -- putStrLn (oldCoords)
+--   -- let newGrid = switchCoordinates (1, 2)  (2, 2) puzzle2
+--   -- putStrLn "Modified Grid:"
+--   -- mapM_ putStrLn newGrid
+
+--   let char1 = 'H'
+--       char2 = 'L'
+--       coordinates1 = findCharCoordinates char1 puzzle2
+--       coordinates2 = findCharCoordinates char2 puzzle2
+--   putStrLn ("Coordinates of '" ++ [char1] ++ "': " ++ show coordinates1)
+--   putStrLn ("Coordinates of '" ++ [char2] ++ "': " ++ show coordinates2)
+
+--   -- let newGrid = switchCoordinates coordinates1[0] coordinates2[0] puzzle2
+--   let newGrid = switchCoordinates coordinates1[0] coordinates2[0] puzzle2
+--   putStrLn "Modified Grid:"
+--   -- mapM_ putStrLn newGrid
+
+
+
+
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FunctionalDependencies #-}
 
 -- Define a type class for structures that can contain characters
 class CharacterContainer a where
   findCharCoordinates :: Char -> a -> [(Int, Int)]
-  switchCoordinates :: (Int, Int) -> (Int, Int) -> a -> a
+  switchCoordinates :: Char -> Char -> a -> a
 
 -- Implement the CharacterContainer type class for a list of strings
 instance CharacterContainer [String] where
   findCharCoordinates char grid =
     [(row, col) | (row, rowString) <- zip [0..] grid, (col, gridChar) <- zip [0..] rowString, gridChar == char]
   
-  switchCoordinates (row1, col1) (row2, col2) grid =
-    let char1 = (grid !! row1) !! col1
-        char2 = (grid !! row2) !! col2
-        newRow1 = replaceAtIndex col1 (grid !! row1) char2
-        newRow2 = replaceAtIndex col2 (grid !! row2) char1
-        newGrid = replaceAtIndex row1 grid newRow1
-    in replaceAtIndex row2 newGrid newRow2
+  switchCoordinates char1 char2 grid =
+    case (findCharCoordinates char1 grid, findCharCoordinates char2 grid) of
+      ([coord1], [coord2]) ->
+        let (row1, col1) = coord1
+            (row2, col2) = coord2
+            newRow1 = replaceAtIndex col1 (grid !! row1) char2
+            newRow2 = replaceAtIndex col2 (grid !! row2) char1
+            newGrid = replaceAtIndex row1 grid newRow1
+        in replaceAtIndex row2 newGrid newRow2
+      _ -> grid  -- If either char1 or char2 is not found or multiple occurrences are found, return the original grid
 
 -- Helper function to replace an element at a specific index in a list
 replaceAtIndex :: Int -> [a] -> a -> [a]
@@ -132,25 +196,13 @@ puzzle2 = ["AC DE",
 
 main :: IO ()
 main = do
-  -- let charToFind = 'H'
-      -- coordinates = findCharCoordinates charToFind puzzle2
-  -- putStrLn ("Coordinates of '" ++ [charToFind] ++ "': " ++ show coordinates)
-
-  -- -- let newGrid = switchCoordinates (1, 2) (2, 2) puzzle2
-  -- -- let oldCoords = findCharCoordinates ' ' puzzle2
-  -- -- putStrLn (oldCoords)
-  -- let newGrid = switchCoordinates (1, 2)  (2, 2) puzzle2
-  -- putStrLn "Modified Grid:"
-  -- mapM_ putStrLn newGrid
-
-  let char1 = 'H'
-      char2 = 'L'
+  let char1 = ' '
+      char2 = 'H'
       coordinates1 = findCharCoordinates char1 puzzle2
       coordinates2 = findCharCoordinates char2 puzzle2
   putStrLn ("Coordinates of '" ++ [char1] ++ "': " ++ show coordinates1)
   putStrLn ("Coordinates of '" ++ [char2] ++ "': " ++ show coordinates2)
 
-  -- let newGrid = switchCoordinates coordinates1[0] coordinates2[0] puzzle2
-  let newGrid = switchCoordinates coordinates1[0] coordinates2[0] puzzle2
+  let newGrid = switchCoordinates char1 char2 puzzle2
   putStrLn "Modified Grid:"
-  -- mapM_ putStrLn newGrid
+  mapM_ putStrLn newGrid
