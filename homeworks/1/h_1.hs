9b7ef90a652615fa4bcaadfc672fcb485d9576cb
Vojtěch Průša <prusa.vojtech@gmail.com>
2023-10-13 09:53:07 +0200

    1.1. done

--- homeworks/1/h_1.hs
+++ homeworks/1/h_1.hs
@@ -5,6 +5,9 @@ module Main
 import Control.Monad.RWS (First(getFirst))
 import Data.Binary.Get (isEmpty)
 
+-- https://hackage.haskell.org/package/base-4.19.0.0/docs/Data-List.html#g:15 - partition - split by predicate
+import Data.List (partition)
+
 type Result = [String]
 pp :: Result -> IO ()
 pp x = putStr (concat (map (++"\n") x))
@@ -20,14 +23,6 @@ ex1 = (3, "ab", [(0,'a',1), (0,'b',0), (1,'a',1), (1,'b',2), (2,'a',1), (2,'b',0
 ex2 :: Automaton 
 ex2 = (3, "ab", [(0,'a',1), (0,'a',0), (0,'b',0), (1,'b',2)], 0, [2])
 
--- Helper function to replace an element at a specific index in a list
-replaceAtIndex :: Int -> [a] -> a -> [a]
-replaceAtIndex index xs newElement =
-  take index xs ++ [newElement] ++ drop (index + 1) xs
-
--- Function to apply a function to a value (pipe operator)
-(|>) :: a -> (a -> b) -> b
-(|>) x f = f x
 
 -- Function to print automata to standard output
 printAutomaton :: Automaton -> IO ()
@@ -42,21 +37,36 @@ printAutomaton (states, alphabet, transitions, startState, acceptingStates) = do
         printTransition (from, char, to) = 
             putStrLn $ "(" ++ show from ++ ", " ++ [char] ++ ", " ++ show to ++ ")"
 
+-- Function to decide if an automaton is deterministic
+isDeterministic :: Automaton -> Bool
+isDeterministic (states, alphabet, transitions, startState, acceptingStates) =
+  -- all transitions have to be detemrinistic
+  all isTransitionDeterministic (groupBySameFromAndChar transitions)
+  where
+    -- Check if transitions for a group are deterministic
+    -- Helper function that takes list of transitions and returns Bool that determins if transition has for same start state single input character  
+    isTransitionDeterministic :: [(Int, Char, Int)] -> Bool
+    isTransitionDeterministic group = ((length group) == 1)
 
--- Function to decide if input autmata is deterministic
-isDeterministic:: Automaton  -> Bool
-isDeterministic (states, alphabet, transitions, startState, acceptingStates) = let
-        result = alphabet == ""
-       in
-        result
+-- Function that groups transitions by (from state, input symbol)
+groupBySameFromAndChar :: [Transition] -> [[Transition]]
+groupBySameFromAndChar [] = []
+groupBySameFromAndChar (t : ts) =
+  let (same, rest) = partition (isSameFromAndChar t) ts
+   in (t : same) : groupBySameFromAndChar rest
 
+-- Predicate function to check if two transitions have the same "from" state and input symbol
+isSameFromAndChar :: Transition -> Transition -> Bool
+isSameFromAndChar (from1, char1, _) (from2, char2, _) = from1 == from2 && char1 == char2
 
 main :: IO ()
 main = do
-    putStrLn "Automaton 1: "
-    printAutomaton ex1
-    putStrLn "\nAutomaton 2: "
-    printAutomaton ex2
+  putStrLn "Automaton 1: "
+  printAutomaton ex1
+  putStrLn "\nAutomaton 2: "
+  printAutomaton ex2
 
-    putStrLn ("Is #1 deterministic?: " ++ (if isDeterministic ex1 then "True" else "False"))
+  putStrLn ("Is #1 deterministic?: " ++ (if isDeterministic ex1 then "True" else "False"))
+  putStrLn ("Is #2 deterministic?: " ++ (if isDeterministic ex2 then "True" else "False"))
+
 
