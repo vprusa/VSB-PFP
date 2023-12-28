2cdf791f7f57a73f365a1c0e7d0191bce845ece5
Vojtěch Průša <prusa.vojtech@gmail.com>
2023-12-28 16:14:07 +0100

    PFP proj - list users with cart items

--- homeworks/proj/main.hs
+++ homeworks/proj/main.hs
@@ -95,26 +95,60 @@ createTestUsersItems users = map (\(User userId nick name) -> createItemsForUser
 -- findUserByName :: String -> [User] -> Maybe User
 -- findUserByName name users = head $ filter (\(User id nick mail) -> nick == name ) users 
 
+-- with userNick find Just user or Nothing 
 findUserByName :: String -> [User] -> Maybe User
 findUserByName name users = 
     case filter (\u -> userNick u == name) users of
         [] -> Nothing
         (x:_) -> Just x
 
-
+-- converts foud user to string
 findUserByNameString :: String -> [User] -> String
 findUserByNameString name users = show (findUserByName name users)
+
+-- filter items by user id
+findUserItemsByUserId :: Int -> [Item] -> [Item]
+findUserItemsByUserId userId items = filter (\(Item itemId itemUserId itemName) -> itemUserId == userId) items
+  -- (findUserByName name users)
+
+-- getUserId :: Maybe User -> Int
+-- getUserId user = case user of
+--         Just (User) -> User
+--         Nothing -> -1
+--         -- userId user
+
+getUserId :: Maybe User -> Int
+getUserId maybeUser = case maybeUser of
+    Just user -> userId user
+    Nothing   -> -1
+
+itemsToStr :: [Item] -> String
+itemsToStr items = show $ map (\i -> TL.pack $ show i) items 
+
+-- getUserIdSafe :: Maybe User -> String
+-- -- getUserIdSafe user = userId user
+-- getUserIdSafe maybeUser = case maybeUser of
+--     Just user -> Just (userId user)
+--     Nothing   -> "" -- Nothing
+
+-- getUserIdSafe :: Maybe User -> String
+-- -- getUserIdSafe user = userId user
+-- getUserIdSafe maybeUser = case maybeUser of
+--     Just user -> Just (userId user)
+--     Nothing   -> "" -- Nothing
+
 
 main :: IO ()
 main = scotty 3000 $ do
     let 
       testUsers = createTestUsers
-      testItems = createTestUsersItems testUsers -- TODO persistency?
+      testItems = concat $ createTestUsersItems testUsers -- TODO persistency?
     -- get "/:name" $ do
     get "/" $ do
         -- name <- param "name"
         -- name <- queryParam "name"
-        html $ mconcat ["<h1>Home Page for Users and their Carts!</h1>", 
+        html $ mconcat [
+          "<h1>Home Page for Users and their Carts!</h1>", 
           "<br><a href='/users'> list all users</a>",
           "<br><a href='/items'> list all items</a>"
           ]
@@ -128,10 +162,18 @@ main = scotty 3000 $ do
     -- http://localhost:3000/user/?name=test
     get "/user/:name" $ do
       name <- queryParam "name"
-      let userStr = findUserByNameString name testUsers
+      let userStr = TL.pack $ findUserByNameString name testUsers
+      -- let userId = getUserId $ head $ getUserId $ findUserByName name testUsers
+      let userId = getUserId $ findUserByName name testUsers 
+      -- let userId = getUserId $ head $ getUserId user --  $ findUserByName name testUsers
+      -- let userItemsStr = getUserIdSafe user
+      let userItemsStr = TL.pack $ itemsToStr $ findUserItemsByUserId userId testItems -- testItems
+      -- let userItemsStr = case user of
+        -- Just user -> "" -- Just (userId user)
+        -- Nothing   -> ""
+        -- if not user then "" else "" -- TL.pack $ findUserItemsByUserId user is
       html $ mconcat ["<h1>User Info</h1>", 
         "<br>Searched Name: ", TL.pack name,
-        -- "<br>Found: ", (TL.pack (show (findUserByName (show name) testUsers) ++ "<br>"))
         -- "<br>Found: ", (TL.pack (show (findUserByName "user-1" testUsers) ++ "<br>"))
         -- "<br>Found: ", (TL.pack (show (findUserByName (show name) testUsers) ++ "<br>"))
         -- "<br>Found: ", (TL.pack (show (findUserStringByName (show name) testUsers) ++ "<br>"))
@@ -139,7 +181,8 @@ main = scotty 3000 $ do
         -- "<br>Found: ", (TL.pack (show (findUserStringByName (show name) testUsers) ++ "<br>"))
         -- "<br>Found: ", (TL.pack (show (findUserStringByName ( show $ TL.pack $ (show name) ) testUsers ) ++ "<br>"))
         -- "<br>Found: ", (TL.pack (show (findUserStringByName ( show name ) testUsers ) ++ "<br>"))
-        "<br>Found: ", TL.pack userStr,  "<br>"
+        "<br>Found: ", userStr,  "<br>",
+        "<br>Cart items: ", userItemsStr,  "<br>"
         -- "<br>Found: ", ( TL.pack $ show $ typeOf name)
         -- "<br>Found: ", ( TL.pack $ show $ typeOf name)
         -- "<br>Found: ", (TL.pack (show (findUserByName name testUsers) ++ "<br>"))] --mconcat (map (\i -> TL.pack (show i ++ "<br>") ) testItems)
